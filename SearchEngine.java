b66210b89d53bc8eaffb9c2be7aa6c9aa9e110f0
kavyasubramanyam <114467463+kavyasubramanyam@users.noreply.github.com>
2022-10-06 11:35:44 -0700

    Create SearchEngine.java
    Search Engine file

---
+++ SearchEngine.java
@@ -0,0 +1,38 @@
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    String listOfStrings[] = new String[100];
+    int index = 0;
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return ("Done!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                index ++;
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    listOfStrings[index] += parameters[1];
+                    return ("Success! "+ parameters[1] + "added");
+                }
+            }
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                String searchFor = "";
+                if (parameters[0].equals("s")) {
+                    searchFor = parameters[1];
+                }
+                for(String n : listOfStrings){
+                    if(n.contains(searchFor)){
+                        System.out.println(n);
+                    }
+                }
+        }
+        return "404 Not Found!";
+    }
+}
+}
